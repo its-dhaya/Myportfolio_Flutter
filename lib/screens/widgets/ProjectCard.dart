82e72369ac61bc7d10183f41f2d45ed12c0882d0
codescraving <you@example.com>
2024-07-14 06:25:49 +0530

    sixth commit

--- lib/screens/widgets/ProjectCard.dart
+++ lib/screens/widgets/ProjectCard.dart
@@ -10,59 +10,69 @@ class Projectcard extends StatefulWidget {
 }
 
 class _ProjectcardState extends State<Projectcard> {
-
   bool _isHovered = false;
+
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return MouseRegion(
-      onEnter: (_)=>_onHovered (true),
-      onExit: (_)=>_onHovered (false),
-
+      onEnter: (_) => _onHovered(true),
+      onExit: (_) => _onHovered(false),
       child: Container(
-        height: size.width*0.08,
-        width: size.width*0.08,
-        decoration: BoxDecoration(color: colors.valhalla.withOpacity(0.8),
-        borderRadius: BorderRadius.circular(15)
+        height: size.width * 0.08,
+        width: size.width * 0.08,
+        decoration: BoxDecoration(
+          color: colors.valhalla.withOpacity(0.8),
+          borderRadius: BorderRadius.circular(15),
         ),
-      child: Stack(
-        children: [
-        Container(
-          
-          margin: EdgeInsets.all(50),
-          
-          child: Image.asset('assets/images/chattify.jpeg'),
-        ),
-        Align(
-          alignment: Alignment.center,
-          child: AnimatedContainer(duration:  Duration(microseconds: 300),
-          curve: Curves.bounceIn,
-          height: _isHovered?size.width:0,
-          width: size.width,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: colors.studio
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(15),
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(child: Center(
-                    child: TextWidget(sSize: size,text: "Chat App",
-                    size: 18,
-                    ),
-                  ),)
-                ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Stack(
+            children: [
+              // Adjust the size and fit of the image here
+              Image.asset(
+                'assets/images/chattify.jpeg',
+                fit: BoxFit.cover, // Choose the fit that suits your needs
+                width: double.infinity, // Use double.infinity to fill parent width
+                height: double.infinity, // Use double.infinity to fill parent height
               ),
-            ),
+              Align(
+                alignment: Alignment.center,
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 300), // Corrected microseconds to milliseconds
+                  curve: Curves.bounceIn,
+                  height: _isHovered ? size.width : 0,
+                  width: size.width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: colors.studio,
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            child: Center(
+                              child: TextWidget(
+                                sSize: size,
+                                text: "Chat App",
+                                size: 18,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
-          ),
-        )
-      ],),
+        ),
       ),
     );
   }
@@ -71,7 +81,5 @@ class _ProjectcardState extends State<Projectcard> {
     setState(() {
       _isHovered = isHovered;
     });
-  
+  }
 }
-}
-
--- lib/screens/widgets/socialwidget.dart
+++ lib/screens/widgets/socialwidget.dart
@@ -1,111 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:myportfolio/constants/colors.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class Socialwidget extends StatefulWidget {
-  const Socialwidget({Key? key}) : super(key: key);
 
-  @override
-  _SocialwidgetState createState() => _SocialwidgetState();
-}
-
-class _SocialwidgetState extends State<Socialwidget> {
-  // Track hover state for each icon
-  bool _isLinkedInHovered = false;
-  bool _isGitHubHovered = false;
-  bool _isFigmaHovered = false;
+class Socialwidget extends StatelessWidget {
+  const Socialwidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        _buildIconButton(
-          FontAwesomeIcons.linkedinIn,
-          'LinkedIn',
-          _isLinkedInHovered,
-          () {
-            // Add your LinkedIn onPressed action here
-            print('LinkedIn icon pressed');
-          },
-          () {
-            setState(() {
-              _isLinkedInHovered = !_isLinkedInHovered;
-            });
-          },
-        ),
-        SizedBox(width: 10,),
-        _buildIconButton(
-          FontAwesomeIcons.github,
-          'GitHub',
-          _isGitHubHovered,
-          () {
-            // Add your GitHub onPressed action here
-            print('GitHub icon pressed');
-          },
-          () {
-            setState(() {
-              _isGitHubHovered = !_isGitHubHovered;
-            });
-          },
-        ),
-        SizedBox(width: 10,),
-        _buildIconButton(
-          FontAwesomeIcons.figma,
-          'Figma',
-          _isFigmaHovered,
-          () {
-            // Add your Figma onPressed action here
-            print('Figma icon pressed');
-          },
-          () {
-            setState(() {
-              _isFigmaHovered = !_isFigmaHovered;
-            });
-          },
-        ),
-      ],
-    );
-  }
 
-  Widget _buildIconButton(IconData icon, String tooltip, bool isHovered, VoidCallback onPressed, VoidCallback onHover) {
-    return MouseRegion(
-      onEnter: (_) {
-        onHover();
-      },
-      onExit: (_) {
-        onHover();
-      },
-      child: Container(
+      Container(
+
         height: 40,
         width: 40,
         decoration: BoxDecoration(
           color: Colors.transparent,
           shape: BoxShape.circle,
-          border: Border.all(color: Colors.white.withOpacity(0.5)),
-          boxShadow: isHovered ? [
-            BoxShadow(
-              color: colors.paleSlate.withOpacity(0.5),
-              blurRadius: 8,
-              spreadRadius: 2,
-              offset: Offset(0, 4),
-            ),
-          ] : [],
+          border: Border.all(color: colors.studio.withOpacity(0.5))
         ),
-        child: Tooltip(
-          message: tooltip,
+        child: Center(
           child: IconButton(
-            onPressed: onPressed,
+            hoverColor: colors.paleSlate,
+            onPressed: (){
+              launchUrl(Uri.parse('https://www.linkedin.com/in/dhayanithisr/'));
+            },
             icon: FaIcon(
-              icon,
-              color: isHovered ? colors.paleSlate : colors.studio,
+              FontAwesomeIcons.linkedinIn,
+              color: colors.studio,
               size: 15,
             ),
           ),
         ),
       ),
+
+        const SizedBox(width: 10,),
+        Container(
+
+          height: 40,
+          width: 40,
+          decoration: BoxDecoration(
+              color: Colors.transparent,
+              shape: BoxShape.circle,
+              border: Border.all(color: colors.studio.withOpacity(0.5))
+          ),
+          child: Center(
+            child: IconButton(
+              hoverColor: colors.paleSlate,
+              onPressed: (){
+                launchUrl(Uri.parse('https://github.com/its-dhaya?tab=repositories'));
+              },
+              icon: FaIcon(
+                FontAwesomeIcons.github,
+                color: colors.studio,
+                size: 15,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(width: 10,),
+        Container(
+
+          height: 40,
+          width: 40,
+          decoration: BoxDecoration(
+              color: Colors.transparent,
+              shape: BoxShape.circle,
+              border: Border.all(color: colors.studio.withOpacity(0.5))
+          ),
+          child: Center(
+            child: IconButton(
+              hoverColor: colors.paleSlate,
+              onPressed: (){
+                launchUrl(Uri.parse('https://www.figma.com/files/team/1337789596594089491/project/210589236/Mine?fuid=1337789593886478175'));
+              },
+              icon: FaIcon(
+                FontAwesomeIcons.figma,
+                color: colors.studio,
+                size: 15,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
-
